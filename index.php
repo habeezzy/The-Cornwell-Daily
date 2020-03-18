429cfdd7e77395553ed07cd466bffeebca06c045
habeezzy <mhabeebulah@gmail.com>
2020-03-18 14:22:56 -0400

    html css template complete

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <title>The Cornwell Daily</title>
+    <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/style.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <h1>
+          <a href="index.html">The Cornwell Daily</a>
+          <small>Advanced Wordpress Theme</small>
+        </h1>
+      <div class="h_right">
+        <form>
+          <input type="text" placeholder='Search...'>
+        </form>
+      </div>
+      </div>
+    </header>
+    <nav class='nav main-nav'>
+      <div class="container">
+          <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="#">Blog</a></li>
+          </ul>
+      </div>
+    </nav>
+    <div class="container content">
+      <div class="main block">
+      <article class="post">
+        <h2>Latest Coronavirus Outlook</h2>
+        <p class="meta">Posted at 8:00 on March 14th by CDC</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque lectus tellus, ut egestas lectus pellentesque lobortis. Mauris imperdiet justo sapien, id interdum felis tincidunt consectetur. Phasellus mi erat, convallis vel euismod et, euismod nec dolor. Duis et faucibus metus, non rutrum tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean pharetra tempus ipsum, id egestas augue. Cras tortor metus, aliquet eget blandit quis, porta vestibulum sapien. Nam neque risus, ornare sed magna et, porttitor euismod enim. Mauris congue et massa a condimentum. Sed nec efficitur nunc. Sed ultrices blandit tristique. Donec pellentesque finibus arcu sit amet euismod. Nullam lacinia, risus in aliquet pharetra, erat tortor pulvinar ligula, quis mattis quam purus ut nulla.
+
+Suspendisse non nisl metus. Nullam commodo facilisis justo a dictum. Fusce vel massa non diam commodo maximus. Suspendisse at ipsum non leo consectetur aliquet. Nunc pulvinar, mi vel dignissim tristique, ipsum risus egestas urna, a porttitor sem felis a risus. Integer nec iaculis dolor, vel mollis tellus. Maecenas quis magna facilisis neque posuere finibus non et nibh.
+
+Donec eleifend imperdiet metus eu lacinia. Donec vel aliquet ipsum. Fusce orci nunc, laoreet ut vulputate at, molestie nec nibh. Suspendisse pharetra mattis commodo. Cras dui ipsum, posuere lacinia dapibus vitae, rutrum sed massa. Nullam sollicitudin leo vitae euismod auctor. Pellentesque nibh ligula, euismod at justo sed, vehicula mollis nibh. Quisque fringilla tempus nibh, sit amet pharetra metus egestas eget. Suspendisse potenti. Fusce auctor elit vitae neque posuere tempus. Aliquam ultrices, ipsum vitae vulputate bibendum, velit nulla mollis erat, vitae tristique magna purus eu nibh. Mauris rutrum dui at augue feugiat, vel tempus mauris mollis. Ut nec ex risus.</p>
+        <a class="button" href="#">Read More</a>
+      </article>
+      <article class="post">
+        <h2>Protecting Your Investments 2020</h2>
+        <p class="meta">Posted at 10:00 on May 9th by admin</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque lectus tellus, ut egestas lectus pellentesque lobortis. Mauris imperdiet justo sapien, id interdum felis tincidunt consectetur. Phasellus mi erat, convallis vel euismod et, euismod nec dolor. Duis et faucibus metus, non rutrum tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean pharetra tempus ipsum, id egestas augue. Cras tortor metus, aliquet eget blandit quis, porta vestibulum sapien. Nam neque risus, ornare sed magna et, porttitor euismod enim. Mauris congue et massa a condimentum. Sed nec efficitur nunc. Sed ultrices blandit tristique. Donec pellentesque finibus arcu sit amet euismod. Nullam lacinia, risus in aliquet pharetra, erat tortor pulvinar ligula, quis mattis quam purus ut nulla.
+
+Suspendisse non nisl metus. Nullam commodo facilisis justo a dictum. Fusce vel massa non diam commodo maximus. Suspendisse at ipsum non leo consectetur aliquet. Nunc pulvinar, mi vel dignissim tristique, ipsum risus egestas urna, a porttitor sem felis a risus. Integer nec iaculis dolor, vel mollis tellus. Maecenas quis magna facilisis neque posuere finibus non et nibh.
+
+Donec eleifend imperdiet metus eu lacinia. Donec vel aliquet ipsum. Fusce orci nunc, laoreet ut vulputate at, molestie nec nibh. Suspendisse pharetra mattis commodo. Cras dui ipsum, posuere lacinia dapibus vitae, rutrum sed massa. Nullam sollicitudin leo vitae euismod auctor. Pellentesque nibh ligula, euismod at justo sed, vehicula mollis nibh. Quisque fringilla tempus nibh, sit amet pharetra metus egestas eget. Suspendisse potenti. Fusce auctor elit vitae neque posuere tempus. Aliquam ultrices, ipsum vitae vulputate bibendum, velit nulla mollis erat, vitae tristique magna purus eu nibh. Mauris rutrum dui at augue feugiat, vel tempus mauris mollis. Ut nec ex risus.</p>
+        <a class="button" href="#">Read More</a>
+      </article>
+      <article class="post">
+        <h2>5 Ways to Avoid Burnout</h2>
+        <p class="meta">Posted at 10:00 on May 9th by admin</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque lectus tellus, ut egestas lectus pellentesque lobortis. Mauris imperdiet justo sapien, id interdum felis tincidunt consectetur. Phasellus mi erat, convallis vel euismod et, euismod nec dolor. Duis et faucibus metus, non rutrum tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean pharetra tempus ipsum, id egestas augue. Cras tortor metus, aliquet eget blandit quis, porta vestibulum sapien. Nam neque risus, ornare sed magna et, porttitor euismod enim. Mauris congue et massa a condimentum. Sed nec efficitur nunc. Sed ultrices blandit tristique. Donec pellentesque finibus arcu sit amet euismod. Nullam lacinia, risus in aliquet pharetra, erat tortor pulvinar ligula, quis mattis quam purus ut nulla.
+
+Suspendisse non nisl metus. Nullam commodo facilisis justo a dictum. Fusce vel massa non diam commodo maximus. Suspendisse at ipsum non leo consectetur aliquet. Nunc pulvinar, mi vel dignissim tristique, ipsum risus egestas urna, a porttitor sem felis a risus. Integer nec iaculis dolor, vel mollis tellus. Maecenas quis magna facilisis neque posuere finibus non et nibh.
+
+Donec eleifend imperdiet metus eu lacinia. Donec vel aliquet ipsum. Fusce orci nunc, laoreet ut vulputate at, molestie nec nibh. Suspendisse pharetra mattis commodo. Cras dui ipsum, posuere lacinia dapibus vitae, rutrum sed massa. Nullam sollicitudin leo vitae euismod auctor. Pellentesque nibh ligula, euismod at justo sed, vehicula mollis nibh. Quisque fringilla tempus nibh, sit amet pharetra metus egestas eget. Suspendisse potenti. Fusce auctor elit vitae neque posuere tempus. Aliquam ultrices, ipsum vitae vulputate bibendum, velit nulla mollis erat, vitae tristique magna purus eu nibh. Mauris rutrum dui at augue feugiat, vel tempus mauris mollis. Ut nec ex risus.</p>
+        <a class="button" href="#">Read More</a>
+      </article>
+      </div>
+      <div class="side">
+        <div class="block">
+          <h3>Health Tips</h3>
+          <p>mi ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget felis eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat</p>
+          <a class="button">More</a>
+        </div>
+      </div>
+    </div>
+    <footer class="main-footer">
+      <div class="container">
+        <div class="f_left">
+          <p> &copy; 2020 - Advanced WP Theme </p>
+        </div>
+        <div class="f_right">
+          <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="#">Blog</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+
+  </body>
+</html>
